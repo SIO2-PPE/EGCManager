3ff06d878b8e481d67ba748e191723bf19a86d2a
Max RENAUD <contact@max-renaud.fr>
2020-12-01 16:52:37 +0100

    Application_Startup

--- Direction/App.xaml.cs
+++ Direction/App.xaml.cs
@@ -13,5 +13,10 @@ namespace Direction
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Login wnd = new Login();
+            wnd.Show();
+        }
     }
 }
