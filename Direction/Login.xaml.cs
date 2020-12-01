b3da20e1f01db5bd7aa9d3a7f158abf068c45d20
Max RENAUD <contact@max-renaud.fr>
2020-12-01 16:48:33 +0100

    Login

---
+++ Direction/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Direction
+{
+    /// <summary>
+    /// Logique d'interaction pour Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Planning subWindow = new Planning();
+            // subWindow.Show();
+            this.Close();
+        }
+    }
+}
