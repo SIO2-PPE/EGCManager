8a59e89486745d635308aa3ef8b2339dd3a5f546
Max RENAUD <contact@max-renaud.fr>
2020-12-02 18:55:02 +0100

    Add id réservation

--- Model/Business/Reservation.cs
+++ Model/Business/Reservation.cs
@@ -6,13 +6,15 @@ namespace Model.Business
 {
     public class Reservation : IHydrate
     {
+        private int _id;
         private int _montant;
         private DateTime _date;
         private Client _client;
         private Partie _partie;
 
-        public Reservation(int montant, DateTime date, Client client, Partie partie)
+        public Reservation(int id,int montant, DateTime date, Client client, Partie partie)
         {
+            _id = id;
             _montant = montant;
             _date = date;
             _client = client;
@@ -20,6 +22,7 @@ namespace Model.Business
         }
         public Reservation()
         {
+            _id = 0;
             _montant = 0;
             _date = new DateTime();
             _client = new Client();
@@ -27,6 +30,7 @@ namespace Model.Business
         }
         public Reservation(Client client, int montant)
         {
+            _id = 0;
             _montant = montant;
             _date = DateTime.Now;
             _client = client;
@@ -37,8 +41,10 @@ namespace Model.Business
         public DateTime Date { get => _date; set => _date = value; }
         public Client Client { get => _client; set => _client = value; }
         public Partie Partie { get => _partie; set => _partie = value; }
+        public int Id { get => _id; set => _id = value; }
         public void Hydrate(Dictionary<string, dynamic> val)
         {
+            _id = val["id"];
             _montant = val["montant"];
             _date = val["date"];
             _client = val["client"];
@@ -48,6 +54,7 @@ namespace Model.Business
         public Dictionary<string, dynamic> ToArray()
         {
             Dictionary<string, dynamic> val = new Dictionary<string, dynamic>();
+            val.Add("id", _id);
             val.Add("montant", _montant);
             val.Add("date", _date);
             val.Add("client", _client);
