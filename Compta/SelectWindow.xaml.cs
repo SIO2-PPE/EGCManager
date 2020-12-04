2529052d83ff639ff1e93cb6e6a6ca2bf74cd750
Mael <mael.chanliau@saintmichelannecy.Fr>
2020-12-04 15:36:10 +0100

    Commit 15h35

---
+++ Compta/SelectWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PPE_1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class SelectWindow : Window
+    {
+        public SelectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                SaisieClient subWindow = new SaisieClient();
+                subWindow.Show();
+                this.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            {
+                CreationClient subWindow = new CreationClient();
+                subWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Compta/viewModel/viewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compta.viewModel
+{
+    class viewModelBase
+    {
+    }
+}
